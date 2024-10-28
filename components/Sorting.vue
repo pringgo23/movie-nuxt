be8ef6bc245a60fa63e9c5f24c9e6c84895aaf7f
pringgo23 <pringgo@perqara.com>
2024-10-28 10:28:58 +0700

    style: make sorting feature

---
+++ components/Sorting.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="bg-gray-800 text-white w-64 rounded-lg shadow-md">
+    <div class="mb-6">
+      <label class="block text-lg font-semibold mb-2 px-4 pt-5 pb-4">Sort Result By</label>
+      <div id="border-solid" class="h-0.5 border-t border-solid border-white border-opacity-10" />
+      <div class="px-3 pt-4 pb-7">
+          <select
+            class="w-11/12 bg-gray-700 text-white p-3 rounded border border-gray-600 focus:outline-none focus:border-gray-400"
+            :value="sortBy"
+            @input="$emit('update:sortBy', $event.target.value)"
+          >
+            <option value="popularity.asc">Popularity Ascending</option>
+            <option value="popularity.desc">Popularity Descending</option>
+            <option value="release_date.asc">Release Date Ascending</option>
+            <option value="release_date.desc">Release Date Descending</option>
+            <option value="vote_average.asc">Rating Ascending</option>
+            <option value="vote_average.desc">Rating Descending</option>
+          </select>
+      </div>
+    </div>
+    <div id="border-solid" class="h-0.5 border-t border-solid border-white border-opacity-10" />
+    <div>
+      <h3 class="text-lg font-semibold p-4">Genres</h3>
+      <div id="border-solid" class="h-0.5 border-t border-solid border-white border-opacity-10 mb-3" />
+      <div
+        v-for="item in genres.genres"
+        :key="item.id"
+        class="flex space-y-2 w-48 justify-between items-center space-x-2 px-4"
+      >
+        <label :for="item.id" class="text-gray-200">{{ item.name }}</label>
+        <input
+          type="checkbox"
+          :id="item.id"
+          class="custom-checkbox"
+          :value="selectedGenres"
+          :checked="isChecked(item.id)"
+          @change="toggleGenre(item.id)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Genre {
+  id: number;
+  name: string;
+}
+
+// @ts-ignore
+const props = defineProps<{
+  selectedGenres: any;
+  sortBy: string;
+  genres: Genre;
+}>();
+
+function isChecked(id: number) {
+  return props.selectedGenres.includes(id);
+}
+
+function toggleGenre(id: number) {
+  const newSelectedGenres = [...props.selectedGenres];
+
+  if (newSelectedGenres.includes(id)) {
+    // If genre ID is already selected, remove it
+    newSelectedGenres.splice(newSelectedGenres.indexOf(id), 1);
+  } else {
+    // Otherwise, add the genre ID
+    newSelectedGenres.push(id);
+  }
+  emit("update:selectedGenres", newSelectedGenres);
+}
+
+// @ts-ignore
+const emit = defineEmits(["update:sortby", "update:selectedGenres"]);
+</script>
+
+<style>
+
+.custom-checkbox {
+  @apply appearance-none w-5 h-5 border-2 border-gray-400 rounded-md cursor-pointer transition-all duration-300 ease-in-out;
+}
+
+.custom-checkbox:checked {
+  @apply bg-[#E74C3C] border-transparent;
+}
+
+.custom-checkbox:checked::before {
+  content: '✔';
+  color: white;
+  font-size: 0.75rem;
+  text-align: center;
+  display: block;
+  line-height: 1.1rem; /* Centering the checkmark */
+}
+
+.bg-gray-800 {
+  background: linear-gradient(to bottom, #1a1a1a, #2a2a2a);
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,109 @@
 <template>
-  <h1 class="text-secondary text-3xl font-bold underline">{{ title }}</h1>
+  <div>
+    <section>
+      <h1 class="text-secondary text-3xl font-bold underline">{{ title }}</h1>
+    </section>
+
+    <section>
+      <div>
+        <!-- {{ id }} -->
+        <!-- <br /> -->
+        <!-- <input v-model="id" /> -->
+        <!-- {{ selectedGenres }}
+        {{ selectedGenresString }} -->
+
+        <div v-if="status === 'success'" class="flex gap-3">
+          <!-- --- component filter --- -->
+          <Sorting
+            v-model:sortBy="sortBy"
+            v-model:selectedGenres="selectedGenres"
+            :genres="genres"
+          ></Sorting>
+          <div class="grid grid-cols-4">
+            <div
+              v-for="item in data.results"
+              :key="item.id"
+              class="text-center"
+            >
+              <img
+                :src="`https://image.tmdb.org/t/p/w220_and_h330_face/${item.poster_path}`"
+                alt="poster"
+              />
+              {{ item.title }}
+            </div>
+          </div>
+        </div>
+        <div v-else-if="status === 'error'" class="">Error</div>
+        <div v-else class="">Loading...</div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
+const id = ref(null);
+const selectedGenres = ref([]);
+const sortBy = ref("popularity.desc");
+const selectedGenresString = computed(() => selectedGenres.value.join("%2C"));
 
-const title = ref('ini title')
+const headers = {
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiMmYwZWU5MjE3ZDgyOWI3ZGVhYTNiYTI5YTZmODEzYyIsIm5iZiI6MTcyOTk1NzE0My4wNTkyMzUsInN1YiI6IjYyNjI1MmVjMTY4ZWEzMTU1N2QzMzBkMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.UVmWHMpoqD0qCekycgjftOuIQHmf6yzZceERpwWlvRs",
+  Accept: "application/json",
+};
+const { data: genres } = await useFetch(
+  "https://api.themoviedb.org/3/genre/movie/list?language=en",
+  {
+    // @ts-ignore
+    headers,
+    lazy: true,
+    server: false,
+  }
+);
 
+const url = computed(() => {
+  return `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=${sortBy.value}&with_genres=${selectedGenresString.value}`;
+});
+
+const { data, status } = await useFetch(url, {
+  // @ts-ignore
+  headers,
+  lazy: true,
+  server: false,
+  watch: [selectedGenres],
+});
+
+const currentSlide = ref(0);
+const movies = ref([
+  {
+    title: "News of the World",
+    rating: 7.2,
+    year: 2021,
+    genre: "Drama",
+    description: "A Texan traveling across the wild West bringing the news...",
+    image: "path/to/news-of-the-world.jpg",
+  },
+  {
+    title: "Space Sweepers",
+    rating: 7.3,
+    year: 2021,
+    genre: "Sci-Fi",
+    description: "When the crew of a space junk collector ship discovers...",
+    image: "path/to/space-sweepers.jpg",
+  },
+  {
+    title: "To All the Boys: Always and Forever",
+    rating: 8.1,
+    year: 2021,
+    genre: "Drama",
+    description: "Senior year of high school takes center stage as Lara...",
+    image: "path/to/to-all-the-boys.jpg",
+  },
+]);
+
+const setCurrentSlide = (index) => {
+  currentSlide.value = index;
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
